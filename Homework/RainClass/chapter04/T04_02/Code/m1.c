d8ffebcfcd7f476854ed0fc88295349637123c29
Sonder9999 <2734165908@qq.com>
2024-12-22 15:17:24 +0800

    第二题题目代码配置

---
+++ Homework/RainClass/chapter04/T04_02/Code/m1.c
@@ -0,0 +1,8 @@
+/* m1.c*/
+void pl(void);
+int main()
+
+{
+    p1();
+    return 0;
+}
---
+++ Homework/RainClass/chapter04/T04_02/Code/m2.c
@@ -0,0 +1,8 @@
+/*   m2.c     */
+#include <stdio.h>;
+char main[2];
+
+void p1()
+{
+    print("0x%x%x\n", main[0], main[1]);
+}
